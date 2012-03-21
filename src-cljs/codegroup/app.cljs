4bb949d527df6bb962821447adb4234ac3fce994
Paul Bostrom <pbostrom@gmail.com>
2012-03-21 18:26:54 -0500

    more cljs

--- src-cljs/codegroup/app.cljs
+++ src-cljs/codegroup/app.cljs
@@ -17,20 +17,27 @@
 ;          });	
 (def ws-url "ws://localhost:8080/socket")
 (def socket (js/WebSocket. ws-url))
-(add-msg (crate/html [:p.event "Socket Status: " + (str (.-readyState socket))]))
+(add-msg (crate/html [:p#out.event "Socket Status: " + (str (.-readyState socket))]))
 (set! (.-onopen socket)
       #(add-msg 
          (crate/html [:p.event "Socket Status: " + (str (.-readyState socket)) + " (open)"])))
 
 (set! (.-onmessage socket)
-      (fn add-msg [msg]
-         (crate/html [:p.event "Received: " + (.-data msg)])))
+      (fn [msg] (add-msg (crate/html [:p.event "Received: " + (.-data msg)]))))
 
-(event/listen (dm/single-node (dmc/sel "#text"))
-              :keypress
-              (fn [e]
-                (if (= (.-keyCode e) 13)
-                  (send-it))))
+(defn send-it []
+  (let [text-el (goog.dom/getElement "text")
+        text (.-value text-el)]
+    (.send socket text)
+    (add-msg (crate/html [:p.event "Sent: " + text]))
+    (set! (.-value text-el) "")))
+
+(defn enter-cb [e]
+  (if (= (.-keyCode e) 13)
+    (send-it)))
+
+
+(event/listen (dm/single-node (dmc/sel "#text")) :keypress (enter-cb))
 ;          function message(msg){
 ;            $('#chatLog').append(msg+'</p>');
 ;          }

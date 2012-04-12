e228a72360b5577d043b206e907d850a9a38f50b
Paul Bostrom <pbostrom@gmail.com>
2012-04-12 18:20:18 -0500

    Integrating with tryclj

--- src/codegroup/server.clj
+++ src/codegroup/server.clj
@@ -1,6 +1,7 @@
 (ns codegroup.server
-  (:use compojure.core, aleph.core, aleph.http, hiccup.core, hiccup.page, lamina.core)
+  (:use compojure.core, aleph.core, aleph.http, lamina.core)
   (:require [compojure.route :as route])
+  (:require [codegroup.views :as views])
   (:gen-class))
 
 (def broadcast-channel (permanent-channel))
@@ -13,27 +14,15 @@
              (siphon broadcast-channel ch))))
 ;             (siphon ch broadcast-channel))))
 
-(defn layout [& content]
-  (html5 [:head [:title "new page"]
-;          (include-js "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js")
-          (include-css "/css/main.css")]
-         [:body content]
-         (include-js "/js/bootstrap.js")
-         [:script {:type "text/javascript"} "goog.require('myrepl')"]))
 
-(def main-view 
-  [:div#wrapper [:div#container 
-                 [:h1 "WebSockets client"]
-                 [:div#chatLog]
-                 [:input#text {:type "text"}]
-                 [:button#disconnect "Disconnect"]]])
 
 
 (defroutes my-app 
-  (GET "/" [] (layout main-view))
+  (GET "/" [] (views/layout views/main-view))
+  (GET "/tryclj" [] views/tryclj)
   (GET "/socket" [] (wrap-aleph-handler chat-handler))
   (route/resources "/")
-  (route/not-found (layout [:p "aww... this doesn't exist"])))
+  (route/not-found (views/layout [:p "aww... this doesn't exist"])))
 
 (defn -main []
   (start-http-server (wrap-ring-handler my-app) {:port 8080 :websocket true})
